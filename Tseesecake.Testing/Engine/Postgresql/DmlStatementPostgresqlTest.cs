3d59461a3ed150a6bb53834178ccfe967f17e1bb
Cédric L. Charlier <seddryck@gmail.com>
2023-09-16 11:34:22 +0200

    Fix tiny issues in tests for postgresql

--- Tseesecake.Testing/Engine/Postgresql/DmlStatementPostgresqlTest.cs
+++ Tseesecake.Testing/Engine/Postgresql/DmlStatementPostgresqlTest.cs
@@ -10,8 +10,8 @@ namespace Tseesecake.Testing.Engine.Postgresql
         protected override string DialectName => "pgsql";
 
         protected override string CreateOrReplace
-            => "CREATE OR REPLACE TABLE \"WindEnergy\"(\r\n\t\"Instant\" TIMESTAMP NOT NULL\r\n\t, \"WindPark\" VARCHAR(50) NOT NULL\r\n\t, \"Producer\" VARCHAR(50) NOT NULL\r\n\t, \"Forecasted\" DECIMAL(15, 6) NOT NULL\r\n\t, \"Produced\" DECIMAL(15, 6) NOT NULL\r\n);\r\n";
+            => "DROP TABLE IF EXISTS \"WindEnergy\";\r\nCREATE TABLE \"WindEnergy\"(\r\n\t\"Instant\" TIMESTAMP NOT NULL\r\n\t, \"WindPark\" VARCHAR(50) NOT NULL\r\n\t, \"Producer\" VARCHAR(50) NOT NULL\r\n\t, \"Forecasted\" DECIMAL(15, 6) NOT NULL\r\n\t, \"Produced\" DECIMAL(15, 6) NOT NULL\r\n);\r\n";
         protected override string CopyFrom
-            => $"{CreateOrReplace}\r\nCOPY\r\n\t\"WindEnergy\"\r\nFROM\r\n\t'.\\..\\..\\..\\WindEnergy.csv'\r\nWITH (\r\n\tFORMAT CSV\r\n\t, DELIMITER ','\r\n\t, HEADER 1\r\n\t, TIMESTAMPFORMAT '%d/%m/%Y %H:%M:%S'\r\n);\r\n";
+            => $"{CreateOrReplace}\r\nSET DateStyle TO euro;\r\n\r\nCOPY\r\n\t\"WindEnergy\"\r\nFROM\r\n\t'.\\..\\..\\..\\WindEnergy.csv'\r\nWITH (\r\n\tFORMAT CSV\r\n\t, DELIMITER ','\r\n\t, HEADER 1\r\n\t, TIMESTAMPFORMAT '%d/%m/%Y %H:%M:%S'\r\n);\r\n";
     }
 }
