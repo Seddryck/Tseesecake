8e389ccb08b54e6a9d1a7a1c7998597fca115c94
Cédric L. Charlier <seddryck@gmail.com>
2023-09-18 22:27:37 +0200

    Arranger to handle having clauses referencing projections

---
+++ Tseesecake/Engine/MsSql/MsSqlServerArrangerCollectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using DubUrl.Querying.Dialects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tseesecake.Arrangers;
+using Tseesecake.Modeling;
+
+namespace Tseesecake.Engine.MsSqlServer
+{
+    [Dialect<TSqlDialect>()]
+    internal class MsSqlServerArrangerCollectionFactory : BaseArrangerCollectionFactory
+    {
+        protected override ISelectArranger[] InstantiateDialect()
+            => new[] { new ExpressionForwardedToHavingClause() };
+    }
+}
