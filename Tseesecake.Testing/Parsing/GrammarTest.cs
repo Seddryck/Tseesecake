7efd3c1b223db36f95959ff4553afdf245b9f352
Cédric L. Charlier <seddryck@gmail.com>
2023-10-02 09:16:49 +0200

    feat: add handling of escaped characters, by duplication, in double/single/backtick quoted text

--- Tseesecake.Testing/Parsing/GrammarTest.cs
+++ Tseesecake.Testing/Parsing/GrammarTest.cs
@@ -14,14 +14,19 @@ namespace Tseesecake.Testing.Parsing
         [TestCase("f")]
         [TestCase("F")]
         [TestCase("foo")]
+        [TestCase(" foo ")]
         [TestCase("FooBar")]
         [TestCase("FooBar123")]
         [TestCase("Foo_Bar")]
         [TestCase("_bar")]
         [TestCase("\"foo\"")]
         [TestCase("\"_foo123\"")]
+        [TestCase("\"foo bar\"")]
+        [TestCase(" \"foo bar\" ")]
+        [TestCase("\" foo bar \"")]
+        [TestCase("\"foo  \"\" bar\"")]
         public void Identifier_ValidIdentifier_Successful(string id)
-            => Assert.That(Grammar.Identifier.Parse(id), Is.EqualTo(id.Replace("\"", "")));
+            => Assert.That(Grammar.Identifier.Parse(id), Is.EqualTo(id.Trim().Replace("\"\"", "\0\0").Replace("\"", "").Replace("\0\0", "\"")));
 
         [Test]
         [TestCase("1")]
@@ -35,5 +40,16 @@ namespace Tseesecake.Testing.Parsing
         [TestCase("foo,bar")]
         public void Identifier_NotValidIdentifier_NotSuccessful(string id)
             => Assert.That(Grammar.Identifier.Parse(id), Is.Not.EqualTo(id));
+
+        [Test]
+        [TestCase("\"foo\"")]
+        [TestCase(" \"foo\" ")]
+        [TestCase("\"\"\"foo\"")]
+        [TestCase("\"foo\"\"\"")]
+        [TestCase("\"foo\"\"bar\"")]
+        [TestCase("\"foo\"\"\"\"bar\"")]
+        [TestCase("\"foo\"\"\"\"bar\"")]
+        public void DoubleQuotedTextual_Valid_Text(string str)
+            => Assert.That(Grammar.DoubleQuotedTextual.Parse(str), Is.EqualTo(str.Trim()[1..^1].Replace("\"\"", "\"")));
     }
 }
